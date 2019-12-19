7a6001b48670e3ddcb3e752cbf8bc63e43f32243
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-19 12:58:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,13 +1,28 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+binding.pry
 def directors_totals(nds)
+ # puts nds[0][:name] this prints steven spielberg
+
+
+#directors = nds[0][:name]
+  
+#   count = 0
+  
+#   while count < directors.length 
+#     print  "#{directors[count][:name]}"
+#     count += 1
+#   end
+  
+
+end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
+  result {
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -18,5 +33,6 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+  
+  
+  https://repl.it/repls/OffshoreMiserlyPaintprogram
